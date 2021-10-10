c40c43513a705f0cf9b2c03e9a808dfaab94fe2c
Roi Martin <jroi.martin@gmail.com>
2021-10-10 15:43:41 +0200

    expos: move kernel entrypoint to function

--- expos/src/main.rs
+++ expos/src/main.rs
@@ -4,10 +4,16 @@
 #![no_main]
 #![feature(panic_info_message)]
 
+use range::RangeSet;
 use uefi::acpi;
 
 mod panic;
 mod serial;
+
+struct BootInfo {
+    available_memory: RangeSet,
+    acpi_madt: acpi::Madt,
+}
 
 /// UEFI entry point.
 #[no_mangle]
@@ -18,44 +24,40 @@ extern "C" fn efi_main(
     // Initialize serial.
     serial::init_serial();
 
+    // Parse UEFI's system table.
     let system_table =
         unsafe { uefi::SystemTable::new(system_table_ptr).unwrap() };
 
-    println!("{:#x?}", system_table);
-
-    let boot_services = system_table.boot_services().unwrap();
-
-    println!("{:#x?}", boot_services);
-
-    let (memory_map, map_key) =
-        uefi::mem::get_available_memory(&boot_services).unwrap();
-    println!("memory_map: {:#x?} {:#x}", memory_map.ranges(), map_key);
-    println!("available memory: {}", memory_map.size());
-
+    // Get LAPIC data.
     let config_tables = system_table.configuration_tables().unwrap();
-
-    println!("{:#x?}", config_tables);
-
     let rsdp20_ptr = config_tables.acpi_rsdp20_ptr().unwrap();
     let rsdp20 = unsafe { acpi::Rsdp20::new(rsdp20_ptr).unwrap() };
-
-    println!("{:#x?}", rsdp20);
-
     let xsdt = rsdp20.xsdt().unwrap();
-
-    println!("{:#x?}", xsdt);
-
     let madt = xsdt.madt().unwrap();
 
-    println!("{:#x?}", madt);
+    // Get available memory.
+    let boot_services = system_table.boot_services().unwrap();
+    let (available_memory, map_key) =
+        uefi::mem::get_available_memory(&boot_services).unwrap();
 
-    let lapic = madt.lapic();
-
-    println!("{:#x?}", lapic);
-
+    // Exit UEFI boot services.
     boot_services
         .exit_boot_services(image_handle, map_key)
         .unwrap();
+
+    // Fill `BootInfo` structure and call kernel's entrypoint.
+    let boot_info = BootInfo {
+        available_memory,
+        acpi_madt: madt,
+    };
+    os_main(boot_info)
+}
+
+/// Kernel entry point.
+fn os_main(boot_info: BootInfo) -> ! {
+    println!("lapic: {:#x?}", boot_info.acpi_madt.lapic());
+    println!("memory map: {:#x?}", boot_info.available_memory.ranges());
+    println!("memory size: {}", boot_info.available_memory.size());
 
     panic!("end");
 }
